187ed088ec5b30011fcedf124b0c0d4556a9b4fa
keqiongpan <keqiongpan@163.com>
2015-11-23 04:20:25 +0800

    创建OCExtension工程，主要输出OCExtension.h和OCExtension.m两个代码文件，用于扩展或简化使用Objective-C/Swift编程的能力。

---
+++ Projects/OCExtension/OCExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCExtension
+//
+//  Created by stduser on 15/11/23.
+//  Copyright © 2015年 SVS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Projects/OCExtension/OCExtension/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCExtension
+//
+//  Created by stduser on 15/11/23.
+//  Copyright © 2015年 SVS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
