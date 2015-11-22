4058aa4d23c08aa46b9c5297dd61699632936d9b
keqiongpan <keqiongpan@163.com>
2015-11-23 04:24:09 +0800

    添加调试器相关操作、系统信号相关操作、内存访问相关操作、对象原型相关操作。

--- Projects/OCExtension/OCExtension/ViewController.m
+++ Projects/OCExtension/OCExtension/ViewController.m
@@ -7,21 +7,16 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "OCExtension.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (is_debugging()) {
+        self.view.backgroundColor = [UIColor redColor];
+    }
 }
 
 @end
